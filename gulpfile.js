f322889e47363293645b11d04c0099226b82dd21
typer <teiqeo@gmail.com>
2020-11-02 20:51:49 -0800

    factored out simple popup

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ sass.compiler = require("dart-sass");
 //the order of files is important
 const gulpSrc = [
   "./src/js/firebase-functions.js",
+  "./src/js/simple-popup.js",
   "./src/js/misc.js",
   "./src/js/words.js",
   "./src/js/layouts.js",
