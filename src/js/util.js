8cfd429a16e7fb2e1ff2b97043f942b836df105d
typer <teiqeo@gmail.com>
2020-11-02 21:26:32 -0800

    moved updateFavicon() to util.js

--- src/js/util.js
+++ src/js/util.js
@@ -40,3 +40,43 @@ function applyColorfulMode(tc) {
     $("#words").removeClass("colorfulMode");
   }
 }
+
+function updateFavicon(size, curveSize) {
+  let maincolor, bgcolor;
+
+  bgcolor = getComputedStyle(document.body)
+    .getPropertyValue("--bg-color")
+    .replace(" ", "");
+  maincolor = getComputedStyle(document.body)
+    .getPropertyValue("--main-color")
+    .replace(" ", "");
+
+  if (bgcolor == maincolor) {
+    bgcolor = "#111";
+    maincolor = "#eee";
+  }
+
+  var canvas = document.createElement("canvas");
+  canvas.width = size;
+  canvas.height = size;
+  let ctx = canvas.getContext("2d");
+  ctx.beginPath();
+  ctx.moveTo(0, curveSize);
+  //top left
+  ctx.quadraticCurveTo(0, 0, curveSize, 0);
+  ctx.lineTo(size - curveSize, 0);
+  //top right
+  ctx.quadraticCurveTo(size, 0, size, curveSize);
+  ctx.lineTo(size, size - curveSize);
+  ctx.quadraticCurveTo(size, size, size - curveSize, size);
+  ctx.lineTo(curveSize, size);
+  ctx.quadraticCurveTo(0, size, 0, size - curveSize);
+  ctx.fillStyle = bgcolor;
+  ctx.fill();
+  ctx.font = "900 " + (size / 2) * 1.2 + "px Roboto Mono";
+  ctx.textAlign = "center";
+  ctx.fillStyle = maincolor;
+  ctx.fillText("mt", size / 2 + size / 32, (size / 3) * 2.1);
+  // document.body.appendChild(canvas);
+  $("#favicon").attr("href", canvas.toDataURL("image/png"));
+}
